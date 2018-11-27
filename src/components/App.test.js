c9a6db155175f859d648c3749166296ff6abde9e
Patro <code@patrickrompf.de>
2018-11-27 18:19:06 +0100

    Add memory router to app test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { mount } from 'enzyme';
@@ -9,7 +10,9 @@ it('renders without crashing', () => {
   const store = createStore(rootReducer);
   mount(
     <Provider store={store}>
-      <App />
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
     </Provider>
   );
 });
