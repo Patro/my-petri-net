8a149d880ec6f71293ecd342924a6b52e706d358
Patro <code@patrickrompf.de>
2018-11-27 16:17:17 +0100

    Add selector to query outgoing edges of node

--- src/selectors/petriNet.js
+++ src/selectors/petriNet.js
@@ -1,3 +1,7 @@
 export const getIncomingEdges = (state, nodeId) => (
   Object.values(state.edgesById).filter(edge => edge.to == nodeId)
 );
+
+export const getOutgoingEdges = (state, nodeId) => (
+  Object.values(state.edgesById).filter(edge => edge.from == nodeId)
+);
