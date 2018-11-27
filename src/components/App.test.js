49e4d190104262e1e19aae704a0f86d69b471320
Patro <code@patrickrompf.de>
2018-11-27 17:58:32 +0100

    Add redux store to app test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { mount } from 'enzyme';
+import rootReducer from './../reducers';
+import App from './App';
 
 it('renders without crashing', () => {
-  mount(<App />);
+  const store = createStore(rootReducer);
+  mount(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
