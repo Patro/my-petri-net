5c853b408fd64d26315518443cf38946926a6816
Patro <code@patrickrompf.de>
2018-12-18 23:17:23 +0100

    Add simulator container to app component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from 'antd';
 import EditorContainer from '../containers/EditorContainer';
+import SimulatorContainer from '../containers/SimulatorContainer';
 import Sidebar from './Sidebar'
 import Topbar from './Topbar';
 import './App.css';
@@ -15,6 +16,7 @@ class App extends Component {
           <Layout>
             <Topbar />
             <Route path="/:id/edit" component={EditorContainer} />
+            <Route path="/:id/simulate" component={SimulatorContainer} />
           </Layout>
         </Layout>
       </>
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,35 +1,50 @@
 import React from 'react';
-import { StaticRouter } from 'react-router-dom';
+import MockRouter from 'react-mock-router';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import configureStore from 'redux-mock-store'
 import { mount } from 'enzyme';
-import rootReducer from './../reducers';
 import EditorContainer from '../containers/EditorContainer';
+import SimulatorContainer from '../containers/SimulatorContainer';
 import App from './App';
+
+const mountApp = (props) => (
+  mount(
+    <Provider store={props.store}>
+      <MockRouter location={props.location}>
+        <App />
+      </MockRouter>
+    </Provider>
+  )
+);
+const setupState = () => ({
+  petriNets: ['petri-net-id'],
+  petriNetsById: {
+    'petri-net-id': {
+      id: 'petri-net-id',
+      edgesById: {},
+      nodesById: {},
+      markings: [],
+    },
+  },
+});
+const setupStore = () => ( configureStore()(setupState()) );
 
 describe('App', () => {
   it('should render editor container if mode is edit', () => {
-    const state = {
-      petriNetsById: {
-        'petri-net-id': {
-          id: 'petri-net-id',
-          edgesById: {},
-          nodesById: {},
-          markings: [],
-        },
-      },
-    };
-    const store = createStore(rootReducer, state);
+    const store = setupStore();
 
-    const wrapper = mount(
-      <Provider store={store}>
-        <StaticRouter location={{ pathname: "/petri-net-id/edit" }} context={{}}>
-          <App />
-        </StaticRouter>
-      </Provider>
-    );
+    const wrapper = mountApp({ store, location: { pathname: "/petri-net-id/edit" } });
     const editorContainer = wrapper.find(EditorContainer);
 
     expect(editorContainer.length).toBe(1);
+  });
+
+  it('should render simulator container if mode is simulate', () => {
+    const store = setupStore();
+
+    const wrapper = mountApp({ store, location: { pathname: "/petri-net-id/simulate" } });
+    const simulatorContainer = wrapper.find(SimulatorContainer);
+
+    expect(simulatorContainer.length).toBe(1);
   });
 });
